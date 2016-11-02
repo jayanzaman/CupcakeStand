22fb44530da072a6cbddbd66c6e4f74e1eb8c959
Jayan Zaman <jayanzaman@gmail.com>
2016-11-02 12:45:31 -0400

    Added the clock

--- script.js
+++ script.js
@@ -18,18 +18,11 @@ $(function(){
           return newbatch;
         }, 2000);
     } else{
-        alert("No more space in the cupcake display case.")
+        alert("Not enouch space in the cupcake display case.")
         var newbatch = $('<div class="cupcake'+totCupcakes+'""></div>');
           $container.append(newbatch);
         return totCupcakes;
     }
-
-
-
-
-
-
-
   }
 
   var updateCash = function(add, minus){
@@ -47,16 +40,79 @@ $(function(){
     $('span.numOfCC').text($cupcakesLeft);
     return $cupcakesLeft;
   }
+  var customerCreation = function(n){
+
+    var creatCustomer = $('<div class="customer'+n+'""></div>');
+    $container.append(creatCustomer);
+
+
+  }
+  // customerCreation(1);
+  var startTime = function(){
+    var timeDisplay = $('<div class="time">Time: <span class="hour">8</span> Hours <span class="minute">0</span> minutes left</div>');
+    $container.append(timeDisplay);
+
+    var minuteArray = [];
+    for(var i = 0; i<60; i++){
+      minuteArray.push(i);
+    }
+    minuteArray.reverse();
+    console.log(minuteArray);
+    var hourArray = [];
+    for(var j=8; j> -1; j--){
+      hourArray.push(j);
+    }
+    console.log(hourArray);
+    // console.log(hourArray[0]);
+
+      //   var h = 0;
+      // setInterval(function(){
+      //   if(h <hourArray.length){
+      //       console.log(hourArray[h]);
+      //       $('span.hour').text(hourArray[h]);
+      //       h++;
+      //   }
+      // }, 60000);
+        var m = 0;
+        var h = 0;
+      setInterval(function(){
+        if(m <minuteArray.length){
+            console.log(minuteArray[m]);
+            $('span.minute').text(minuteArray[m]);
+            m++;
+        }else{
+          m = 0;
+          // setInterval(function(){
+            if(h < hourArray.length){
+              console.log(hourArray[h]);
+              $('span.hour').text(hourArray[h]);
+              h++;
+            }else{
+              alert("Game finished");
+              m = minuteArray.length;
+            }
+          // }, 60000);
+        }
+      }, 10);
 
 
 
+  }
+  startTime();
 
+    // var startTheGame = function(){
+      $('div.click_btn').on('click', function(){
+        bakeCupcake(6);
+        startTime();
+      })
+    // }
+    // startTheGame();
 
 
     //Clicking the "bake new batch initializes when page loads"
-    $('div.click_btn').on('click', function(){
-        bakeCupcake(6);
-    })
+    // $('div.click_btn').on('click', function(){
+    //     bakeCupcake(6);
+    // })
 
 
 
